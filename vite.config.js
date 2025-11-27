c2037c7d41b0d8607bbb93ed5815004d2443c023
Dilashma R. <108285809+Dilashmar@users.noreply.github.com>
2025-11-26 22:04:54 -0500

    Set base path for Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,5 @@
 import { defineConfig, loadEnv  } from 'vite'
 import react from '@vitejs/plugin-react'
-
-
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
@@ -10,5 +8,6 @@ export default defineConfig(({ mode }) => {
       'process.env.SOME_KEY': JSON.stringify(env.SOME_KEY)
     },
     plugins: [react()],
+    base: '/InkAndChat/'
   }
 })
